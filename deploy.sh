87ffde7ffdffd54957db67dcfe039da8f7e59ee4
NiFink <Nils@team-fink.de>
2024-03-15 11:25:21 +0100

    q

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-abort on errors
+#abort on errors
 set -e
 
-env PUBLIC_URL=https://nifink.github.io/Homepage/ npm run build
+env PUBLIC_URL=https://nifink.github.io/homepage npm run build
 
 cd build
 
@@ -11,8 +11,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-deploying to https://{USERNAME}.github.io (see https://pages.github.com for instructions)
-git push -f git@github.com:{USERNAME}/{USERNAME}.github.io.git main
+#deploying to https://{USERNAME}.github.io (see https://pages.github.com for instructions)
+#git push -f git@github.com:{USERNAME}/{USERNAME}.github.io.git main
 
-git push -f git@github.com:TomHerman11/host-react-app-on-github-pages.git main:gh-pages
-```
+git push -f git@github.com:nifink.github.io/homepage.git main:gh-pages
