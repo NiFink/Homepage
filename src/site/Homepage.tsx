4f5a002a9211a7b228cc9f5d8af485e26f68612f
NiFink <Nils@team-fink.de>
2024-03-15 19:42:29 +0100

    q

--- src/site/Homepage.tsx
+++ src/site/Homepage.tsx
@@ -19,7 +19,7 @@ function Homepage({aboutmeClick, projectsClick}: HomepagePops){
                     <img src="homepage/AboutMe.jpg" className="d-block w-100" alt="..." onClick={aboutmeClick}></img>
                 </div>
                 <div className="carousel-item">
-                    <img src="/homepage/src/images/AboutMe.JPG" className="d-block w-100" alt="..." onClick={projectsClick}></img>
+                    <img src="homepage/src/images/AboutMe.jpg" className="d-block w-100" alt="..." onClick={projectsClick}></img>
                 </div>
                 <div className="carousel-item">
                     <img src="src\images\MyWebsite.jpg" className="d-block w-100" alt="..." onClick={aboutmeClick}></img>
