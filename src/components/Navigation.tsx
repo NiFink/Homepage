0f2f289f1894b66daf4f5067836d4334eb8f8d12
NiFink <Nils@team-fink.de>
2024-03-29 12:17:07 +0100

    Navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,12 +3,11 @@ interface NavigationProps {
     projectClick?: () => void;
     contactClick?: () => void;
 }
-
 function Navigation({ homepageClick, projectClick, contactClick }: NavigationProps) {
     return (
         <div> 
             <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-                <div className="container-fluid justify-content-center">
+                <div className="container-fluid">
                     <button className="btn" onClick={homepageClick}>
                         <img src="./images/Logo.jpeg" alt="NilsFink" width="40" height="40" />
                     </button>
@@ -17,12 +16,12 @@ function Navigation({ homepageClick, projectClick, contactClick }: NavigationPro
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav text-center">
+                        <ul className="navbar-nav ">
                             <li className="nav-item">
-                                <button className="nav-link active" aria-current="page" onClick={homepageClick}>Home</button>
+                                <button className="nav-link active w-100" aria-current="page" onClick={homepageClick}>Home</button>
                             </li>
                             <li className="nav-item">
-                                <button className="nav-link"  onClick={projectClick}>Projects</button>
+                                <button className="nav-link w-100" onClick={projectClick}>Projects</button>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="https://github.com/NiFink">Github</a>
@@ -31,7 +30,7 @@ function Navigation({ homepageClick, projectClick, contactClick }: NavigationPro
                                 <a className="nav-link" href="https://gitlab.mi.hdm-stuttgart.de/">GitLab</a>
                             </li>
                             <li className="nav-item">
-                                <button className="nav-link disabled" aria-disabled="true" onClick={contactClick}>Contact</button>
+                                <button className="nav-link disabled w-100" aria-disabled="true" onClick={contactClick}>Contact</button>
                             </li>
                         </ul>
                     </div>
@@ -42,3 +41,4 @@ function Navigation({ homepageClick, projectClick, contactClick }: NavigationPro
 }
 
 export default Navigation;
+
